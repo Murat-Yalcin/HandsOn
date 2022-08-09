7d199490aad22aeb1dd963a014a81c3b932177d6
Murat Yalcin <yalcinmurat2@gmail.com>
2022-08-09 21:53:46 +0300

    first commit

---
+++ 20220809-javascript/App.js
@@ -0,0 +1,58 @@
+// console.log('App js Running')
+
+// var a = 5;
+// console.log(a);
+
+// {
+//     let a = 3
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// console.log(typeof 0)
+// console.log(typeof 3.14)
+// console.log(typeof 'Hello')
+// console.log(typeof Symbol('hello'))
+// console.log(typeof (8<10))
+// console.log(8 < 10)
+// console.log(typeof 1234567890123456789012345678901234567890n)
+
+// prompt'tan girilen değer string olarak gelir. prompt'un başına + eklenirse değer rakama çevrilir.
+// let b = +prompt('Bir girdi yapınız')
+
+// console.log(b, typeof b)
+
+// let m = 42
+// console.log(typeof m)
+// console.log(typeof typeof m)
+
+// string olan sayı ifadesini
+// başına + ekleyip number tipine
+// parseInt(stringVar) ile integer number a
+// parseFloat(stringVar) ile kesirli sayı
+
+// let age
+
+// console.log(age)
+
+// console.log(myAge)
+
+// var myAge = 40;
+
+// console.log("hello")
+
+// biz bir değişkene bilerek kasıtlı olarak yok değeri vermek istiyorsak "null" kullanabiliriz.
+// değeri verilmemiş değişkenlere bilerek kasıtlı olarak "undefined" KULLNANMAYINIZ
+
+// let a = null
+
+// console.log(typeof a)
+
+let x;
+
+do {
+    x = prompt('enter a Number: ')
+}while (isNaN(x));
+
+console.log(x, x*x)
