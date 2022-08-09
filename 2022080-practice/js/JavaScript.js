95972d51e42be78e2e83df064741b5f71dc2eccb
Murat Yalcin <yalcinmurat2@gmail.com>
2022-08-09 20:13:28 +0300

    some changes

---
+++ 2022080-practice/js/JavaScript.js
@@ -0,0 +1,116 @@
+
+// let aValue;     
+// console.log("1. " + typeof aValue);             //undefined  
+
+// aValue=null;
+// console.log("2. " + typeof aValue);             //object
+
+// aValue=42;
+// console.log("3. " + typeof aValue);             //number
+
+// aValue="Clarusway";
+// console.log("4. " + typeof aValue);             //string
+
+// aValue=true;
+// console.log("5. " + typeof aValue);             //boolean
+
+// aValue = 1234567890123456789012345678901234567890n;
+// console.log("6. " + typeof aValue);             //bigint
+
+// aValue = Symbol("Clarusway");
+// console.log("7. " + typeof aValue);             //symbol
+
+// aValue = { firstName: "John", lastName: "Doe" };
+// console.log("8. " + typeof aValue);             //object
+
+
+
+
+// let age;
+// console.log(age);           //undefined
+// console.log(userName);      //userName is not defined
+
+
+// let x;                      //primitive type---null
+
+
+// let a=7;                    //Numbers
+// console.log(a);
+
+
+// let sezer=5e-9;
+// document.write(sezer);
+
+
+// let c = 999999999999999;        //Noktasız olarak 15 basamaklı sayı yazdırabiliyor
+// let d = 9999999999999999;       //16 basamak olursa hata veriyor
+// console.log(c);
+// console.log(d);
+
+
+// var x = 0.2 + 0.1;
+// console.log(x);        //Noktadan sonra 17.basamağa kadar doğru gösterir.Sonrası 100% doğru olamayabilir
+
+
+// let y = (0.2*10 + 0.1*10) / 10;
+// console.log(y);                     //0.3 verir ve bu şekilde yapılmalıdır.
+
+
+// Hexadecimal
+// let myNumber = 0x5AD;
+// console.log(myNumber);              //1453
+
+
+// Octal
+// let num1 = 015;
+// let num2 = 025;
+// let result = num1 + num2;
+// console.log(`result is ${result}`);                         // result is 34
+
+// num1 = 15;
+// num2 = 025;
+// console.log(`result is ${result}`);                        // result is 36
+
+// num1 = 015;
+// num2 = 25;
+// console.log(`result is ${result}`);                        // result is 38
+
+// num1 = 15;
+// num2 = 25;
+// console.log(`result is ${result}`);                       // result is 40 isn't it weird?
+// let numOctal = 0o1234567;
+
+
+//Binary
+// let myNumber = 0b10011101;
+// console.log(myNumber);                              // myNumber equals to 157 in decimal
+
+
+//Strings
+// let firstName = `John`;
+// let lastName = "Doe";
+// let myCity = 'Tysons';
+// console.log(`I'm ${firstName} ${lastName}. I live in ${myCity}`);
+// console.log("I'm " + firstName + " " + lastName + ". I live in " + myCity);
+    
+
+//Booleans
+// Boolean("Hello World");
+// Boolean(5);
+// Boolean(-5);
+// Boolean("false");
+// Boolean('0');
+// Boolean(3.14 + 8);   //All of these values return true
+
+// Boolean("");
+// Boolean(0);
+// Boolean(-0);
+// Boolean(null);
+// let x;
+// Boolean(x);   //All of these values return false
+
+
+//Objects
+// const myCar = {make: 'Ford', model: 'Mustang', year: 1969}; 
+// document.write(myCar.make);                                         //Ford
+
