198595bb717010db0257ac56efdae002bcaccbe7
Murat Yalcin <yalcinmurat2@gmail.com>
2022-08-11 23:16:50 +0300

    end of practice session

--- 20220811-javascript/app.js
+++ 20220811-javascript/app.js
@@ -45,8 +45,33 @@ console.log('App.js is running.')
 // console.log(today)
 // console.log(today.getFullYear())
 
-const now = new Date();
+// const now = new Date();
 
-console.log(`Day of the month is ${now.getDate()}`);
-console.log(`Month is ${now.getMonth() + 1}`);
-console.log(`Year is ${now.getFullYear()}`);
+// console.log(`Day of the month is ${now.getDate()}`);
+// console.log(`Month is ${now.getMonth() + 1}`);
+// console.log(`Year is ${now.getFullYear()}`);
+
+// let a = +prompt('enter first number');
+// let b = +prompt('enter second number');
+// let c = +prompt('enter third number');
+// let d = (a%10 == b%10) || (a%10 == c%10) || (b%10 == c%10);
+// alert(d);
+
+
+
+// There are at least 2 numbers having same first digit: true/false
+
+// bir cevap
+
+let a = prompt(`Enter the first number: `);
+let b = prompt(`Enter the second number: `);
+let c = prompt(`Enter the third number: `);
+
+const p = (a % 10)
+const q = (b % 10)
+const r = (c % 10)
+
+
+
+const d = ((p == q) || (q == r) || (p == r) )
+console.log(`There are at least 2 numbers having same first digits: ${d}`);
