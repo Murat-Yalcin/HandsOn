1cd4fab07b5da0899f437cff3bf451af58ff4a5e
Murat Yalcin <yalcinmurat2@gmail.com>
2022-08-19 22:30:43 +0300

    first commit

---
+++ 20220819-teamwork/teamwork.js
@@ -0,0 +1,33 @@
+/* """  Boxing yarismasi oyunu:
+2 Player birbirlerine sirayla vurmaya basliyor. İlk baslayani kura ile belirliyorlar. Biri yikilana kadar oyun devam ediyor. Her yumrugun diger oyuncuyu yikma ihtimali 10%. (oyun başladığında tek seferde de yıkabilir)
+
+Output:
+
+Fight!!!
+Player #2 hits
+Player #1 hits
+Player #2 hits
+...
+Player #1 hits and knocks out!
+Game over!!!  """ */
+
+// let player = Math.ceil(Math.random()*2);
+// console.log("--- Game Starts ---");
+// while(true){
+//     if( Math.random() <= 0.1 ){
+//         console.log("Player #" + player + " hits and knocks out!");
+//         console.log("--- Game Over ---");
+//         break;
+//     }
+//     if( player == 1 ){
+//         player = 2;
+//         console.log("Player #1 hits");
+//     }
+//     else{
+//         player = 1;
+//         console.log("Player #2 hits");
+//     }
+// }
+
+// const arr2 = [1, 2, 3, 4];
+// console.log(arr2[Math.floor(Math.random() * arr2.length)]);
