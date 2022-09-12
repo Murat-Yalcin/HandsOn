0203a38ea9479885c68d05da7d31f6af06497794
Murat Yalcin <yalcinmurat2@gmail.com>
2022-09-12 20:18:18 +0300

    first commit

---
+++ 20220910-workshop/javascript.js
@@ -0,0 +1,15 @@
+console.log("********** Workshop **********");
+
+const obj1 = {
+  a: 1,
+  b: {
+    d: 6,
+    e: {
+      f: 3,
+    },
+  },
+};
+
+console.log(obj1.a);
+console.log(obj1.b);
+console.log(obj1.b.e.f);
