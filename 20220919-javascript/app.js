769e9520d210c67c5acd901d04c6abb6efd8abfa
Murat Yalcin <yalcinmurat2@gmail.com>
2022-09-19 21:46:11 +0300

    first commit

---
+++ 20220919-javascript/app.js
@@ -0,0 +1,61 @@
+//* ======================================================
+//*                     TODO APP
+//* ======================================================
+
+//?Selectors
+const addBtn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+
+addBtn.addEventListener("click", () => {
+  if (todoInput.value.trim() === "") {
+    alert("PLease enter new todo");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
+
+    createListElement(newTodo);
+    todoInput.value = "";
+  }
+});
+
+const createListElement = (newTodo) => {
+  //? yeni bir li elementi olustur ve bu elemente obje icerisindeki
+  //? id degerini ve completed class'ini ata
+  const li = document.createElement("li");
+  // li.id = newTodo.id;
+  li.setAttribute("id", newTodo.id);
+
+  //   newTodo.completed ? li.classList.add("completed") : "";
+  newTodo.completed && li.classList.add("completed");
+
+  //? okey ikonu olustur ve li elementine bagla
+  const okIcon = document.createElement("i");
+  okIcon.setAttribute("class", "fas fa-check");
+  li.appendChild(okIcon);
+
+  //? todo basligi icin bir p elementi ve yazi dugumu olusturarak li'ye bagla
+  const p = document.createElement("p");
+  const pTextNode = document.createTextNode(newTodo.text);
+  p.appendChild(pTextNode);
+  li.appendChild(p);
+
+  //? delete ikonu olustur ve li elementine bagla
+  const deleteIcon = document.createElement("i");
+  deleteIcon.setAttribute("class", "fas fa-trash");
+  li.appendChild(deleteIcon);
+  todoUl.appendChild(li);
+};
+
+todoInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    addBtn.click;
+  }
+});
+
+window.onload = function () {
+  todoInput.focus();
+};
