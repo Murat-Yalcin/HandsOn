7d21962d16b6e8df46e4ced97540c5ab55f48280
Murat Yalcin <yalcinmurat2@gmail.com>
2022-10-10 22:18:40 +0300

    first commit

--- 20221010-react/rc03_module-css/src/components/card/Card.jsx
+++ 20221010-react/rc03_module-css/src/components/card/Card.jsx
@@ -1,13 +1,14 @@
 import Buton from "../buton/Buton";
-import "./Card.css";
+// import "./Card.css";
+import CardStyle from "./card.module.css";
 
 const Card = ({ language, btn, img }) => {
   // console.log(props);
   // const { language, btn, img } = props;  //!destr.
   return (
-    <div className="title">
-      <h1>{language}</h1>
-      <img className="images" src={img} alt="img" />
+    <div>
+      <h1 className={CardStyle["title"]}>{language}</h1>
+      <img className={CardStyle["images"]} src={img} alt="img" />
       <Buton btn={btn} />
     </div>
   );
