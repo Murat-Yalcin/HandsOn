4fadbcd0b8f05f2725c5ff93c6d92a1355e70ad8
Murat Yalcin <yalcinmurat2@gmail.com>
2022-10-12 21:30:04 +0300

    first commit

--- 20221011-react/solution/src/App.js
+++ 20221011-react/solution/src/App.js
@@ -1,14 +1,16 @@
-import "./App.scss";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
+import './App.scss';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-  return (
+  return(
     <div className="App">
-      <Navbar />
-      <Header />
+      <Navbar/>
+      <Header/>
+      <Main/>
     </div>
-  );
-};
+  )
+}
 
 export default App;
