f208951dc1a27b87afa72ffd893f5550467cdcdc
Murat Yalcin <yalcinmurat2@gmail.com>
2022-10-15 15:33:13 +0300

    first commit

---
+++ 20221015-teamwork/src/components/tasks/Tasks.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+const Tasks = ({ arr }) => {
+  const [done, setDone] = useState(false);
+  const style = {
+    borderLeft: done ? "4px solid red" : "none",
+    textDecoration: done ? "line-through" : "none",
+  };
+  return (
+    <>
+      <ul>
+        {arr.map((task) => (
+          <li
+            onClick={() => setDone(!done)}
+            key={Date.now()}
+            style={style}
+            className="bg-white p-3 list-unstyled d-flex justify-content-between "
+          >
+            <span>
+              <strong>{task.task}</strong> <br />
+              {task.date}
+            </span>
+
+            <button
+              onClick={(e) => e.target.parentNode.remove()}
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+            ></button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Tasks;
