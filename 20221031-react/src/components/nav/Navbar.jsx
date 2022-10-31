1a9c2504a25d610c7cc76e3847e9238521229c1f
Murat Yalcin <yalcinmurat2@gmail.com>
2022-10-31 23:04:26 +0300

    changes

--- 20221031-react/src/components/nav/Navbar.jsx
+++ 20221031-react/src/components/nav/Navbar.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.style"
-import Nav, { Brand } from "./Navbar.style";
+import "./Navbar.style";
+import Nav, { Brand ,Menu,MenuLink,Hamburger} from "./Navbar.style";
+import {GiHamburgerMenu} from "react-icons/gi"
 
 const Navbar = () => {
+  const [isOpen,setIsOpen]=useState(false)
   return (
     <Nav justify="space-between" wrap="wrap">
-
-<Brand to="/">
-      
-      <i>{"<Clarusway/>"}</i>
+      <Brand to="/">
+        <i>{"<Clarusway/>"}</i>
         <span>Recipe</span>
-      </Brand>  
-  
-     
+      </Brand>
 
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="register">Register</Link>
-        <Link to="Logout">Logout</Link>
-      </div>
+      <Hamburger onClick={()=>setIsOpen(!isOpen)}>
+        <GiHamburgerMenu/>
+      </Hamburger>
+
+      <Menu isOpen={isOpen} onClick={()=>setIsOpen(false)}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="Logout" onclick={()=>sessionStorage.clear()}>Logout</MenuLink>
+      </Menu>
     </Nav>
   );
 };
