9a13196898ea2bf15f2788c18a3a95113251797a
Murat Yalcin <yalcinmurat2@gmail.com>
2022-11-01 20:46:53 +0300

    changes

--- 20221031-react/src/pages/home/Home.jsx
+++ 20221031-react/src/pages/home/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from "axios"
+import Header from '../../components/header/Header';
 
 const Home = () => {
+  const  APP_ID="4e9f05eb";
+  const  APP_KEY="9b904d703fa0d46a88ce1ac63f29f498";
+  const [query,setQuery]=useState("egg");
+  const [selectedMeal,setSelectedMeal]= useState("breakfast")
+  const [recipes,setRecipes]= useState([])
+  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+  const getData=async()=>{
+    const {data}= await axios(url)
+  
+    setRecipes(data.hits)
+  
+  }
+  console.log(recipes);
+  useEffect(() => {
+    getData()
+  },[])
+
+
   return (
     <div>
-      Home
+      <Header setQuery={setQuery} />
     </div>
   )
 }
