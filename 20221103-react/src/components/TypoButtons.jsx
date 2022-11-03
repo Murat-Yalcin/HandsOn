9dff6c63c4ec1ad0ed241fcc76649a0d0bd70e11
Murat Yalcin <yalcinmurat2@gmail.com>
2022-11-03 20:20:58 +0300

    first commit

---
+++ 20221103-react/src/components/TypoButtons.jsx
@@ -0,0 +1,14 @@
+import Typography from "@mui/material/Typography";
+
+const TypoButtons = () => {
+  return (
+    <div>
+      <Typography variant="h1" component="h2">
+        h1.heading
+      </Typography>
+      ;
+    </div>
+  );
+};
+
+export default TypoButtons;
