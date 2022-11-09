97e8e55bb849ab46dbc4a2eda06f6bb66ab0e4d1
Murat Yalcin <yalcinmurat2@gmail.com>
2022-11-09 20:35:46 +0300

    first commit

---
+++ 20221109-react/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+  console.log("Render => Header Components");
+  return <div className="bg-warning text-center">Header Componenti</div>;
+};
+
+export default Header;
