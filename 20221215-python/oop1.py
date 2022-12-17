e6c181a4d76a6ef93b328651cd78a2d75cf96549
Murat Yalcin <yalcinmurat2@gmail.com>
2022-12-17 16:37:39 +0300

    changes

---
+++ 20221215-python/oop1.py
@@ -0,0 +1,69 @@
+# import os
+# os.system('cls' if os.name == 'nt' else 'clear')
+# print("-----------------------------------")
+
+# def print_types(data):
+#     for i in data:
+#         print(i, type(i))
+
+# test = [122, "victor", [1,2,3], (1,2,3), {1,2,3}, True, lambda x:x]
+
+# print_types(test)
+
+# class Person:
+#     name = "victor"
+#     age = 33 
+
+# person1 = Person()
+# person2 = Person()
+
+# print(person1.name)
+# print(person2.age)
+
+# Person.job = "developer"
+# print(person1.job)
+
+
+# class Person:
+#     company = "clarusway"
+
+# person1 = Person()
+# person2 = Person()
+
+# person1.location = "turkey"
+# person1.company = "tesla"
+# print(person1.location)
+# print(person1.company)
+# print(person2.company)
+
+class Person:
+    company = "clarusway"
+    
+    def test(self):
+        print("test")
+
+person1 = Person()
+person2 = Person()
+
+person1.test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("-----------------------------------")
