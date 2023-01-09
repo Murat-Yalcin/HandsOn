823df81cdd74522e59df7cc89114960f10bd9970
Murat Yalcin <yalcinmurat2@gmail.com>
2023-01-09 20:31:47 +0300

    test

--- 20230107-django/flight/views.py
+++ 20230107-django/flight/views.py
@@ -6,13 +6,12 @@ from rest_framework.permissions import IsAdminUser
 from .permissions import IsStafforReadOnly
 from datetime import datetime, date
 
-
 # Create your views here.
 class FlightView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
     permission_classes = (IsStafforReadOnly,)
-
+    
     def get_serializer_class(self):
         serializer = super().get_serializer_class()
         if self.request.user.is_staff:
@@ -35,16 +34,17 @@ class FlightView(viewsets.ModelViewSet):
 
                 queryset = queryset.union(today_qs)
             return queryset
-
-
+    
+    
 class ReservationView(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+    
     
     def get_queryset(self):
         queryset = super().get_queryset()
         if self.request.user.is_staff:
             return queryset
         return queryset.filter(user= self.request.user)
-
+    
 
