1fac72a253d8ffdcc7255414397e1476d430b087
Murat Yalcin <yalcinmurat2@gmail.com>
2023-02-20 22:31:04 +0300

    changes

--- 20230216-django/pizzas/urls.py
+++ 20230216-django/pizzas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, pizzas
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('pizzas/', pizzas, name='pizzas'),
+
 ]
